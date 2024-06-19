76c6e2f84d62ada6ac39283ee255a5584959902b
tanishq <139997424+Tanishq1604@users.noreply.github.com>
2024-06-20 00:37:23 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,8 +7,8 @@ import { ThemeProvider } from "./provider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Adrian's Portfolio",
-  description: "Modern & Minimal JS Mastery Portfolio",
+  title: "Tanishq's Portfolio",
+  description: "my portfolio",
 };
 
 export default function RootLayout({
